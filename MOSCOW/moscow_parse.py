f4353d44c134a2d624a71ed5e87ba4bad19e2f37
duk2nuk <duk2nuk@gmail.com>
2022-11-14 10:21:22 +0000

    new

--- MOSCOW/moscow_parse.py
+++ MOSCOW/moscow_parse.py
@@ -31,6 +31,31 @@ def search_in_object_dict(obj_name, input_dict):
 		if obj_name == i:
 			value = input_dict[obj_name]["obj_num"]
 	return value
+	
+def normalize_object_dict(input_dict):
+	regex_group_norm = (
+		r"(?P<obj_gr>[\d.])"
+	)
+	old_input_dict = input_dict
+	for key, value in old_input_dict.items():
+		obj_group_ip, obj_group_num = value.values()
+		new_list_obj_ip = []
+		for i in obj_group_ip:
+			i = str(i)
+			result_match = re.search(regex_group_norm, i)
+			if result_match:
+				print(i)
+				new_list_obj_ip.append(i)
+			else:
+				for key_search_match, val_search_match in old_input_dict.items():
+					obj_group_ip_match, obj_group_num_match = val_search_match.values()
+					if key_search_match == i:
+						search_number = obj_group_num_match
+						new_list_obj_ip.append(search_number)
+			input_dict[key]["obj_group_ip"]=new_list_obj_ip		
+	return(input_dict)
+		#result_match = re.search(regex, key_value)
+	#return key
 
 def create_object_group_dict(input_data):
 	regex = (
@@ -59,24 +84,20 @@ def create_object_group_dict(input_data):
 				new_obj_group_ip.append(search_result)
 		object_result_dict[obj_group_name] = {"obj_group_ip":new_obj_group_ip, "obj_group_num":index}
 		index+=1
-	for every in object_result_dict.values():
-		print(every)
-		for ip in every["obj_group_ip"]:
-			print(ip)
-			if type(ip)=="int":
-				pass
-			else:
-				print(ip)
+	
 	return object_result_dict
 
 if __name__ == "__main__":
 	output_obj = open_asa_config("raw_data_replace.txt")
 	obj_dict = create_object_dict(output_obj)
 	obj_group_dict = create_object_group_dict(output_obj)
+	normalize_obj_group_dict = normalize_object_dict(obj_group_dict)
 	#with open("obj_dict.yaml", "w") as obj_dict_yaml_write:
-		yaml.dump(obj_dict,obj_dict_yaml_write)
+		#yaml.dump(obj_dict,obj_dict_yaml_write)
 	#with open("obj_group_dict.yaml", "w") as obj_group_dict_yaml_write:
-		yaml.dump(obj_group_dict,obj_group_dict_yaml_write)
+		#yaml.dump(obj_group_dict,obj_group_dict_yaml_write)
+	with open("obj_group_dict_norm.yaml", "w") as obj_group_dict_norm_yaml_write:
+		yaml.dump(normalize_obj_group_dict,obj_group_dict_norm_yaml_write)
 	#pprint(obj_dict, width=200)
 	#pprint(obj_group_dict, width=200)
 	#with open("astrachan_result_fw.xml", "w") as result_obj_xml:
